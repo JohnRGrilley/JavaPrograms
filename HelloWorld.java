0e9e6f9bba000ce214c3afb88e468b5567a49b47
JohnRGrilley <johnrgrilley@gmail.com>
2019-05-10 23:13:17 +0000

    First java program

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class MyClass {
+/*
+ * This is my first program
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// this is my first program
+		System.out.println("Hello World");
+		
+	}
+
+}
